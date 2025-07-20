e003127384ab7e97835a9d52e86d8a5cbf174c86
Tapan Raut <rinku.tapan.raut@gmail.com>
2025-07-20 14:33:53 +0530

    Initial Commit

---
+++ MatchImage/MatchImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchImageApp.swift
+//  MatchImage
+//
+//  Created by Tapan Raut on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
